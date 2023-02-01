fd0ec0bb88877987c01f1f3f305c0680d2037df9
Jonatas Luiz de Oliveira <contact@jonatasoliveira.dev>
2023-02-01 13:41:41 -0300

    add config

---
+++ dynamic_blueprint.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+dim_blueprint = Blueprint('Dynamic Subdomain route', __name__)
+
+@dim_blueprint.route('/', subdomain="<username>")
+def dynamic(username):
+    return f"This is a {username} blueprint"
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import config as default_config
 
 
 def create_app(config=None) -> Flask:
-    app = Flask(__name__, static_folder="../static", template_folder="../templates")
+    app = Flask(__name__, subdomain_matching=True)
 
     if config is None:
         config = default_config
@@ -16,6 +16,8 @@ def create_app(config=None) -> Flask:
     app.register_blueprint(main_blueprint)
     from sub_blueprint import sub_blueprint
     app.register_blueprint(sub_blueprint)
+    from dynamic_blueprint import dim_blueprint
+    app.register_blueprint(dim_blueprint)
         
     # app = blueprints_factory.register_blueprints(app)
 
