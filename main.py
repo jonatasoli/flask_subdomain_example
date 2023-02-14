1dda7b15b92c269ff3d7ef7dd5cdf4582b45f59a
Jonatas Luiz de Oliveira <contact@jonatasoliveira.dev>
2023-02-14 16:18:52 -0300

    feat: add second domain support

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 import config as default_config
 
+FIRST_DOMAIN = "myfinances.guru"
+SECOND_DOMAIN = "taurusbrothers.pro"
+
 
 def create_app(config=None) -> Flask:
-    app = Flask(__name__, subdomain_matching=True, static_folder='static')
+    app = Flask(__name__, subdomain_matching=True, static_folder='static', static_host=FIRST_DOMAIN)
 
     if config is None:
         config = default_config
@@ -13,15 +16,15 @@ def create_app(config=None) -> Flask:
 
     # Blueprints and API Namespaces
     from root_blueprint import main_blueprint
-
     app.register_blueprint(main_blueprint)
+
     from sub_blueprint import sub_blueprint
-
     app.register_blueprint(sub_blueprint)
+
+    from sub_blueprint import second
+    app.register_blueprint(second)
+
     from dynamic_blueprint import dim_blueprint
-
     app.register_blueprint(dim_blueprint)
-
-    # app = blueprints_factory.register_blueprints(app)
 
     return app
--- sub_blueprint.py
+++ sub_blueprint.py
@@ -1,8 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, redirect
+from functools import partial
 
+FIRST_DOMAIN = "myfinances.guru"
+SECOND_DOMAIN = "taurusbrothers.pro"
 sub_blueprint = Blueprint('Subdomain route', __name__)
+second = Blueprint('second', __name__)
+second_route = partial(second.route, host=SECOND_DOMAIN)
+
 
 
 @sub_blueprint.route('/', subdomain='subdomain')
 def index():
     return 'This is a subdomain blueprint'
+
+
+@second_route('/')
+def second_index():
+    return redirect(f'https://angelica.{FIRST_DOMAIN}')
